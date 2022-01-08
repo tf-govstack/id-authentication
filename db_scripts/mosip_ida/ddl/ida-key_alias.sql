332312acc404f112f36a9cc761c34d494d5d52d5
Puneet Joshi <puneet@mosip.io>
2022-01-08 13:13:01 +0530

    [MOSIP-17836] Synced with latest.

--- db_scripts/mosip_ida/ddl/ida-key_alias.sql
+++ db_scripts/mosip_ida/ddl/ida-key_alias.sql
@@ -1,19 +1,21 @@
 CREATE TABLE ida.key_alias(
-	id character varying(36) NOT NULL,
-	app_id character varying(36) NOT NULL,
-	ref_id character varying(128),
-        cert_thumbprint character varying(100),
-	key_gen_dtimes timestamp,
-	key_expire_dtimes timestamp,
-	status_code character varying(36),
-	lang_code character varying(3),
-	cr_by character varying(256) NOT NULL,
-	cr_dtimes timestamp NOT NULL,
-	upd_by character varying(256),
-	upd_dtimes timestamp,
-	is_deleted boolean DEFAULT FALSE,
-	del_dtimes timestamp,
-	CONSTRAINT pk_keymals_id PRIMARY KEY (id)
+    id character varying(36) NOT NULL,
+    app_id character varying(36) NOT NULL,
+    ref_id character varying(128),
+    key_gen_dtimes timestamp,
+    key_expire_dtimes timestamp,
+    status_code character varying(36),
+    lang_code character varying(3),
+    cr_by character varying(256) NOT NULL,
+    cr_dtimes timestamp NOT NULL,
+    upd_by character varying(256),
+    upd_dtimes timestamp,
+    is_deleted boolean DEFAULT FALSE,
+    del_dtimes timestamp,
+    cert_thumbprint character varying(100),
+    uni_ident character varying(50),
+    CONSTRAINT pk_keymals_id PRIMARY KEY (id),
+    CONSTRAINT uni_ident_const UNIQUE (uni_ident)
 );
 COMMENT ON TABLE ida.key_alias IS 'Key Alias: To maintain a system generated key as alias for the encryption key that will be stored in key-store devices like HSM.';
 COMMENT ON COLUMN ida.key_alias.id IS 'ID: Key alias id is a unique identifier (UUID) used as an alias of the encryption key stored in keystore like HSM (hardware security module).';
